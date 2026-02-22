654f6822da72506d3d0b7982bfe7c46c7b4d92dc
codeingmagic108 <codeingmagic@gmail.com>
2026-02-23 01:14:20 +0530

    update project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import './projects.css'
 
 export default function Projects() {
   return (
     <div>
-      <div class='project'>
-        <div class='porject-list'>hello my dear </div>
+      <div class='projects'>
+        <h2>Portfolio Website</h2><hr class='hrr1'/><hr class='hrr1'/>
+        <div>
+          <ul>
+            <li>* Developed a responsive personal portfolio using React.js, HTML5, CSS3, and JavaScript.</li>
+            <li>* Showcased professional details including resume, projects, and contact form with clean UI/UX design.</li>
+            <li>* Implemented dynamic routing and reusable components for scalability.</li>
+            <li>* Deployed the project on GitHub Pages ensuring accessibility with a live URL.</li>
+            <li>* Integrated contact form and social media links to enhance professional networking.</li>
+          </ul>
+        </div>
+        <h2>To-Do Website</h2><hr class='hrr1'/><hr class='hrr1'/>
+        <div>
+          <ul>
+            <li>* Developed a responsive To-Do application using React.js, HTML5, CSS3, and JavaScript.</li>
+            <li>* Implemented task management features like adding, editing, deleting, and marking tasks as complete.</li>
+            <li>* Designed a clean and intuitive UI with local storage persistence for task data.</li>
+          </ul>
+        </div>
+       <h2>Library Management System</h2><hr class='hrr1'/><hr class='hrr1'/>
+        <div>
+          <ul>
+            <li>* Developed a responsive Library Management System using React.js, HTML5, CSS3, and JavaScript.</li>
+            <li>* Implemented features like adding, editing, deleting, and searching books in the library.</li>
+            <li>* Designed a clean and intuitive UI with local storage persistence for book data.</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
